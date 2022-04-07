d3c85f68e98c4b45917f54109e5179fb4741c681
momoka-toriizuka <momokatoriizuka@gmail.com>
2022-04-07 14:06:34 +0900

    トップ画面・会員登録画面・ログイン画面

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,76 +1,74 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+<div class="before-login">
+    <div class="app-title">
+        <h1 class="your-closet">Your Closet</h1>
+    </div>
+    <div class="auth-form">
+        <form method="POST" action="{{ route('register') }}">
+            @csrf
+            <!-- 名前入力 -->
+            <div class="form-group">
+                <div class="row">
+                    <input id="name" type="text" placeholder="名前"
+                        class="auth-text-box @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}"
+                        required autocomplete="name" autofocus>
+                </div>
+                <div class="row">
+                    @error('name')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
                 </div>
             </div>
+            <!-- メールアドレス入力 -->
+            <div class="form-group">
+                <div class="row">
+                    <input id="email" type="email" placeholder="メールアドレス"
+                        class="auth-text-box @error('email') is-invalid @enderror" name="email"
+                        value="{{ old('email') }}" required autocomplete="email">
+                </div>
+                <div class="row">
+                    @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+            </div>
+            <!-- パスワード入力 -->
+            <div class="form-group">
+                <div class="row">
+                    <input id="password" type="password" placeholder="パスワード"
+                        class="auth-text-box @error('password') is-invalid @enderror" name="password" required
+                        autocomplete="new-password">
+                </div>
+                <div class="row">
+                    @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="row">
+                    <input id="password-confirm" type="password" placeholder="パスワード再入力" class="auth-text-box"
+                        name="password_confirmation" required autocomplete="new-password">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="row">
+                    <button type="submit" class="btn btn-primary">会員登録</button>
+                </div>
+            </div>
+        </form>
+        <div class="row">
+            <a href="{{ route('login') }}" class="auth-link">ログイン</a>
         </div>
     </div>
 </div>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'YourCloset') }}</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body class="body">
+    <div class="content">
+        @yield('content')
+    </div>
+</body>
+
+</html>
