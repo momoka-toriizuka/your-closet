b8885d175c5fafa6a0294b0ba45afef74ad58cf2
momoka-toriizuka <momokatoriizuka@gmail.com>
2022-05-11 13:57:06 +0900

    URLとルート名のグループ化

--- routes/web.php
+++ routes/web.php
@@ -27,31 +27,37 @@ Route::get('/logout', [App\Http\Controllers\Auth\LoginController::class, 'logout
  */
 // アイテム一覧
 Route::get('/items', [App\Http\Controllers\ItemController::class, 'index'])->name('items');
-// アイテム登録フォーム
-Route::get('/item/create-form', [App\Http\Controllers\ItemController::class, 'create'])->name('item.create');
-// アイテム登録
-Route::post('/item/create', [App\Http\Controllers\ItemController::class, 'store'])->name('item.store');
-// アイテム削除
-Route::delete('/item/delete/{item_id}', [App\Http\Controllers\ItemController::class, 'destroy'])->name('item.destroy');
-// アイテム詳細
-Route::get('/item/detail/{item_id}', [App\Http\Controllers\ItemController::class, 'detail'])->name('item.detail');
-// アイテム編集フォーム
-Route::get('/item/update-form/{item_id}', [App\Http\Controllers\ItemController::class, 'edit'])->name('item.edit');
-// アイテム編集
-Route::post('/item/update/{item_id}', [App\Http\Controllers\ItemController::class, 'update'])->name('item.update');
+
+Route::group(['prefix' => '/item', 'as' => 'item.'], function() {
+    // アイテム登録フォーム
+    Route::get('create-form', [App\Http\Controllers\ItemController::class, 'create'])->name('create');
+    // アイテム登録
+    Route::post('create', [App\Http\Controllers\ItemController::class, 'store'])->name('store');
+    // アイテム削除
+    Route::delete('delete/{item_id}', [App\Http\Controllers\ItemController::class, 'destroy'])->name('destroy');
+    // アイテム詳細
+    Route::get('detail/{item_id}', [App\Http\Controllers\ItemController::class, 'detail'])->name('detail');
+    // アイテム編集フォーム
+    Route::get('update-form/{item_id}', [App\Http\Controllers\ItemController::class, 'edit'])->name('edit');
+    // アイテム編集
+    Route::post('update/{item_id}', [App\Http\Controllers\ItemController::class, 'update'])->name('update');
+});
 
 /**
  * タグ
  */
 // タグ一覧
 Route::get('/tags', [App\Http\Controllers\TagController::class, 'index'])->name('tags');
-// タグ登録
-Route::post('/tag/create', [App\Http\Controllers\TagController::class, 'store'])->name('tag.store');
-// タグ削除
-Route::delete('/tag/delete/{tag_id}', [App\Http\Controllers\TagController::class, 'destroy'])->name('tag.destroy');
-// タグ編集フォーム
-Route::get('/tag/update-form/{tag_id}', [App\Http\Controllers\TagController::class, 'edit'])->name('tag.edit');
-// タグ編集
-Route::post('/tag/update/{tag_id}', [App\Http\Controllers\TagController::class, 'update'])->name('tag.update');
 // // タグごとのアイテム一覧
 Route::get('/tag/{tag_id}/items', [App\Http\Controllers\TagController::class, 'itemsOfTag'])->name('items-of-tag');
+
+Route::group(['prefix' => '/tag', 'as' => 'tag.'], function() {
+// タグ登録
+Route::post('create', [App\Http\Controllers\TagController::class, 'store'])->name('store');
+// タグ削除
+Route::delete('delete/{tag_id}', [App\Http\Controllers\TagController::class, 'destroy'])->name('destroy');
+// タグ編集フォーム
+Route::get('update-form/{tag_id}', [App\Http\Controllers\TagController::class, 'edit'])->name('edit');
+// タグ編集
+Route::post('update/{tag_id}', [App\Http\Controllers\TagController::class, 'update'])->name('update');
+});
