897ff1db6fa9177e941d64903168708836e67d75
momoka-toriizuka <momokatoriizuka@gmail.com>
2023-01-02 14:51:45 +0900

    フッターのデザイン作成/CSSの共通化と記載場所の整理

--- resources/views/commons/footer.blade.php
+++ resources/views/commons/footer.blade.php
@@ -1,21 +1,21 @@
 <footer class="footer">
-    <ul class="page-transition">
+    <ul class="menu">
         <!-- アイテム一覧リンク -->
-        <li class="page-transition-btn">
+        <li class="icon">
             <a href="{{ route('items') }}">
-                <img src="../public/storage/02_icon_fashion_t-shirt.png" alt="アイテム">
+                <img src="{{ asset('/storage/02_icon_fashion_t-shirt.png') }}" alt="アイテム">
             </a>
         </li>
         <!-- タグ一覧リンク -->
-        <li class="page-transition-btn">
+        <li class="icon">
             <a href="{{ route('tags') }}">
-                <img src="../public/storage/sharp_icon.png" alt="タグ">
+                <img src="{{ asset('/storage/sharp_icon.png') }}" alt="タグ">
             </a>
         </li>
         <!-- コーデ一覧リンク -->
-        <li class="page-transition-btn">
-            <a href="{{ route('items') }}">
-                <img src="../public/storage/09_icon_fashion_onepiece.png" alt="コーデ">
+        <li class="icon">
+            <a href="">
+                <img src="{{ asset('/storage/09_icon_fashion_onepiece.png') }}" alt="コーデ">
             </a>
         </li>
     </ul>
